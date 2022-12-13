a1f2462fac58d44b8e9e9d862433ff84026f605d
Jun_53 <58911113+jun-05@users.noreply.github.com>
2022-12-13 14:56:48 +0900

    🛠 fix : User Create 수정 - userSchema에서 정의된 props와 create시 props가 일치하지 않아 username이 추가되지 않는것을 수정하였습니다.

--- src/pages/api/auth/signup.js
+++ src/pages/api/auth/signup.js
@@ -14,13 +14,10 @@ export default async function handler(req, res) {
     const checkexisting = await Users.findOne({ email });
     if (checkexisting) return res.status(422).json({ message: 'User Already Exists...!' });
 
-    Users.create(
-      { name: username, email, password: await hash(password, 12) },
-      function (err, data) {
-        if (err) return res.status(404).json({ err });
-        res.status(201).json({ status: true, user: data });
-      }
-    );
+    Users.create({ username, email, password: await hash(password, 12) }, function (err, data) {
+      if (err) return res.status(404).json({ err });
+      res.status(201).json({ status: true, user: data });
+    });
   } else {
     res.status(500).json({ message: 'HTTP method not valid only POST Accepted' });
   }
