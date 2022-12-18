4404a0a73d1c10aa722a0c1cff87853f7fd8e467
Jun_53 <58911113+jun-05@users.noreply.github.com>
2022-12-18 13:21:33 +0900

    ⭐ feat : yup을 사용하여 form값 체크 및 ui 및 값 일부수정 - yup을 설치하여 form validate schema를 생성하여 에러처리를 하였습니다. - 추가적으로, date 타입의 inpute에 placeholder를 설정하였습니다. - taskType의 select 옵션의 값을 대문자로 통일하였습니다.

--- src/features/task/AddTask.tsx
+++ src/features/task/AddTask.tsx
@@ -7,12 +7,23 @@ import taskItemState from './recoil/taskRecoil';
 import { useRecoilState } from 'recoil';
 
 //TODOS
-//validate 만들기 (require로만 할까?)
-//mongoDB에 넣기. uid,id 같이 넣어야..
-//ReactQuery 사용해서 넣기, 가져오기. 전역값. SSG로 해도 될듯?
-//mongoDB에서 가져오기
-//수정하기 -> 값 가져올때 단일값 가져오는 API를 SSR로 가져오고, 수정은 mutate?
 
+//mongoDB에 넣기. uid 같이 넣어야..
+//API/task.add 로 요청. mutate 사용? 이게 맞는거 같은데. 로딩때문에.. -> 훅으로 관리?
+//uid값 넣기
+
+// mongoDB에서 가져오기
+// noTasks 컴포넌트 및 Loading 컴포넌트 추가
+// ReactQuery 사용해서 넣기, 가져오기. 전역값. SSG로 해도 될듯?
+
+// 클릭시 수정하기로 이동 -> 값 가져올때 단일값 가져오는 API를 SSR?로 가져오고, 수정은 mutate?
+
+// Menu버튼 클릭시 사이드바 나타나도록 구현 /
+// Personal 및 Business 옵션 선택할 수 있도록 설정. / Complete는 Form에서 처리하는게 좋을듯?
+// SignOut 버튼 추가
+// 회원가입시 이미지 추가할 수 있게 구현하기 및 Menu버튼에서 이미지 볼 수 있도록 하기.
+
+//권한 없는 사용자 url막기. ( 로그인한 유저는 api/auth 막고. / task.add 및 task.Register 수정할것.)
 const AddTask = () => {
   const [_, setTaskItem] = useRecoilState(taskItemState);
 
--- src/features/task/components/FormSection.tsx
+++ src/features/task/components/FormSection.tsx
@@ -4,6 +4,7 @@ import { MdTitle, MdPlace } from 'react-icons/md';
 import { TbNotes } from 'react-icons/tb';
 import { useRecoilState } from 'recoil';
 import tw from 'twin.macro';
+import { object, string, number } from 'Yup';
 
 import taskItemState from '../recoil/taskRecoil';
 import styles from '../styles/Form.module.css';
@@ -16,19 +17,26 @@ const FormSection = () => {
   const formik = useFormik({
     initialValues: {
       iconName: '',
-      taskType: 'business',
+      taskType: 'Business',
       title: '',
       place: '',
       note: '',
       dueTime: '',
       urgent: 1,
     },
+    validationSchema: object({
+      iconName: string().required(),
+      title: string().required('Required').min(1).max(30, 'Must be less then 20 characters long'),
+      place: string().max(20, 'Must be less then 20 characters long'),
+      note: string().max(30, 'Must be less then 30 characters long'),
+      dueTime: string().required('Required'),
+      urgent: number().max(3, 'Invalid Value').required('Required'),
+    }),
     onSubmit,
   });
 
   function onSubmit(values: task) {
-    values.iconName = iconName;
-    console.log(values);
+    if (formik.errors) values.iconName = iconName;
   }
 
   const animationOption = `${
@@ -38,26 +46,30 @@ const FormSection = () => {
   return (
     <FormSectionBlock className={animationOption} onSubmit={formik.handleSubmit}>
       <select className={styles.select_style} {...formik.getFieldProps('taskType')}>
-        <option value="business">business</option>
+        <option value="business">Business</option>
         <option value="Personal">Personal</option>
       </select>
 
       <select className={styles.select_style} {...formik.getFieldProps('urgent')}>
         <option value={1}>Importaint</option>
-        <option value={2}>moderate </option>
-        <option value={2}>unimportant </option>
+        <option value={2}>Moderate </option>
+        <option value={2}>Unimportant </option>
       </select>
 
       <div className={styles.input_wrapper}>
         <input
           type="text"
-          placeholder="Title"
+          placeholder="title"
           className={styles.input_style_text}
-          {...formik.getFieldProps('title')}
+          name="title"
+          onChange={formik.handleChange}
         />
         <div className={styles.input_icon_wrapper}>
           <MdTitle />
         </div>
+        {formik.errors.title && (
+          <span className={styles.input_error_span}>{formik.errors.title}</span>
+        )}
       </div>
 
       <div className={styles.input_wrapper}>
@@ -65,11 +77,15 @@ const FormSection = () => {
           type="text"
           placeholder="place"
           className={styles.input_style_text}
-          {...formik.getFieldProps('place')}
+          name="place"
+          onChange={formik.handleChange}
         />
         <div className={styles.input_icon_wrapper}>
           <MdPlace />
         </div>
+        {formik.errors.place && (
+          <span className={styles.input_error_span}>{formik.errors.place}</span>
+        )}
       </div>
 
       <div className={styles.input_wrapper}>
@@ -77,19 +93,25 @@ const FormSection = () => {
           type="text"
           placeholder="notification"
           className={styles.input_style_text}
-          {...formik.getFieldProps('note')}
+          name="note"
+          onChange={formik.handleChange}
         />
         <div className={styles.input_icon_wrapper}>
           <TbNotes />
         </div>
+        {formik.errors.note && (
+          <span className={styles.input_error_span}>{formik.errors.note}</span>
+        )}
       </div>
 
       <div className={styles.input_wrapper}>
         <input
           type="datetime-local"
-          placeholder="dueTime"
           className={styles.input_style}
-          {...formik.getFieldProps('dueTime')}
+          data-placeholder="dueTime"
+          name="dueTime"
+          onChange={formik.handleChange}
+          required
         />
       </div>
 
