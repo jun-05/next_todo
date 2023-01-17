877c68937b0d30c69373c24908e96f1909f7b81c
Jun_53 <58911113+jun-05@users.noreply.github.com>
2023-01-17 10:59:02 +0900

    👏 chore : auth 페이지 파일경로 수정 - features/auth/index.ts 를 삭제하여, auth page에서 auth 컴포넌트를 직접 참조하도록 하였습니다.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Auth from '../features/auth';
+import Auth from '../features/auth/Auth';
 
 const Home: NextPage = () => {
   return (
